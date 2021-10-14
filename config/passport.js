c9f1bfbda7a5d26ccd463db696cd38dd8fb473f4
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-10-14 18:38:28 +0530

    Added Signup Feature

--- config/passport.js
+++ config/passport.js
@@ -15,11 +15,11 @@ passport.deserializeUser((id, done) => {
 
 module.exports = function(passport){
   passport.use('local.signup', new LocalStrategy({
-      usernameField: 'email',
+      usernameField: 'username',
       passwordField: 'password',
       passReqToCallback: true
-  }, (req, email, password, done) => {
-        User.findOne({'email': email}, (err, user) => {
+  }, (req, username, password, done) => {
+        User.findOne({'username': username}, (err, user) => {
          if(err){
              return done(err);
          }
@@ -29,13 +29,9 @@ module.exports = function(passport){
           }
 
           const newUser = new User();
-          newUser.name = req.body.name;
-          newUser.phone = req.body.phone;
-          newUser.email = req.body.email;
+          newUser.username = req.body.username;
           newUser.password = newUser.encryptPassword(req.body.password);
-          newUser.description = req.body.artist_desc;
-          newUser.auth_id = 0;
-
+        
           newUser.save((err) => {
                 if(err){
                   return done(err);
@@ -48,17 +44,17 @@ module.exports = function(passport){
 
       //SignIn
       passport.use('local.signin', new LocalStrategy({
-          usernameField: 'email',
+          usernameField: 'username',
           passwordField: 'password',
           passReqToCallback: true
-      }, (req, email, password, done) => {
-            User.findOne({'email': email}, (err, user) => {
+      }, (req, username, password, done) => {
+            User.findOne({'username': username}, (err, user) => {
              if(err){
                  return done(err);
              }
 
               if(!user){
-                  return done(null, false, req.flash('error', 'Invalid Email Address'));
+                  return done(null, false, req.flash('error', 'Invalid Username'));
               }
               if(!user.validPassword(password)){
                           return done(null, false, req.flash('error', 'Invalid Password'));
--- routes/adminSignup.js
+++ routes/adminSignup.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+
+
 // TO GET About PAGE (this or app)
 router.get('/',function(req,res){
-    res.render('admin/signUp');
+    var messages = req.flash('error');
+    res.render('admin/signUp',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 })
+
+
+router.post('/', passport.authenticate('local.signup',{
+    successRedirect: "/admin/login",
+    failureRedirect: "/admin/signup",
+    failureFlash: true
+  }));
+
 
 module.exports = router;
