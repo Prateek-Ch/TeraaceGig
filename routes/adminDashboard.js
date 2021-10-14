9691dff046f252d40612c67df111050957002d42
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-10-14 23:23:19 +0530

    Added Admin Prevention

--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 // TO GET About PAGE (this or app)
-router.get('/',function(req,res){
+router.get('/',isLoggedIn,function(req,res){
     res.render('admin/adminDashboard');
 })
 
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+    if(req.user){
+      console.log(req.user);
+      return next();
+    }
+    else{
+      res.redirect('/admin/login');
+    }
+}
