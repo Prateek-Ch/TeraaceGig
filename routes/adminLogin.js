847e9e2e750a08c31947d875583646b9db28b5c2
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-10-14 18:48:39 +0530

    Added Login

--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
 
-// TO GET About PAGE (this or app)
-router.get('/',function(req,res){
-    res.render('admin/adminLogin');
-})
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+
+router.get('/',function(req,res,next){
+    var messages = req.flash('error');
+    res.render('admin/adminLogin',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/', passport.authenticate('local.signin',{
+    failureRedirect: "/admin/login",
+    failureFlash: true
+  }),(req,res)=>{
+     res.redirect('/admin/dashboard');
+  });
+
 
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    }
+    res.redirect('/');
+  }
+  
+  function notLoggedIn(req, res, next){
+    if(!req.isAuthenticated()){
+      return next();
+    }
+    res.redirect('/');
+  }
+  
