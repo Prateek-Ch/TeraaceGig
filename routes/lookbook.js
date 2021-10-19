5d188908b1275097867a7149a460eeff489520bc
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-10-19 14:57:53 +0530

    Fixed Minor Issues

--- routes/lookbook.js
+++ routes/lookbook.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var Cart = require('../models/cart');
+
+
 // TO GET About PAGE (this or app)
 router.get('/',function(req,res){
-    res.render('lookbook');
+    if(!req.session.cart){
+        return res.render('lookbook',{cart_products: null});
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('lookbook',{cart_products: cart.generateArray(),totalPrice: cart.totalPrice});
 })
 
 module.exports = router;
